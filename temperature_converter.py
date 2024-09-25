744dde45cd6e042c65647b7ed8de4ebae1f5874d
RAILEY C. GUINTO <raileyguinto2@gmail.com>
2024-09-25 20:11:21 +0800

    Added conversion type

--- temperature_converter.py
+++ temperature_converter.py
@@ -6,12 +6,16 @@
             #3.	Perform the appropriate conversion and print the result.
 
 
-
+print('='*30)
 print("Welcome to Temperature Converter!\n")
 
 # Ask the user to input a temperature.
-input('Enter temperature: \n')
+initial_temperature = float(input('Enter temperature: '))
 
 # Ask the user to select the conversion type: from Celsius to Fahrenheit or from Fahrenheit to Celsius.
+print('CONVERSION TYPE\n')
+conversion_type = int(input(f"1. Celcius to Fahrenheit \n"
+                      f"2. Fahrenheit to Celcius\n"
+                      f"Enter here: "))
 # Perform the appropriate conversion
 # Display result
