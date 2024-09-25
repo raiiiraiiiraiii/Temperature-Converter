7bc4995953b4b1f67ff92aaa9c568379afed3471
RAILEY C. GUINTO <raileyguinto2@gmail.com>
2024-09-25 20:27:52 +0800

    Celcius to Fahrenheit

--- temperature_converter.py
+++ temperature_converter.py
@@ -13,9 +13,17 @@ print("Welcome to Temperature Converter!\n")
 initial_temperature = float(input('Enter temperature: '))
 
 # Ask the user to select the conversion type: from Celsius to Fahrenheit or from Fahrenheit to Celsius.
-print('CONVERSION TYPE\n')
+print('\nCONVERSION TYPE\n')
 conversion_type = int(input(f"1. Celcius to Fahrenheit \n"
-                      f"2. Fahrenheit to Celcius\n"
-                      f"Enter here: "))
+                            f"2. Fahrenheit to Celcius\n"
+                            f"Enter here: "))
+
 # Perform the appropriate conversion
+# Celcius to Fahrenheit
+if conversion_type == 1:
+    celcius_to_fahrenheit = (initial_temperature * 9/5) + 32
+    print(f'Celcius: {initial_temperature}°C')
+    print(f'Fahrenheit: {celcius_to_fahrenheit:.2f}°F')
+# Fahrenheit to Celcius
+
 # Display result
